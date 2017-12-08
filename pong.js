124f41fd7ba15cee22a37115ae26cf6834ce2a6f
arrianne <arrianneoshea@hotmail.co.uk>
2017-12-08 16:53:15 +0000

    The paddles now move along the y axis depending on which button is pressed

--- pong.js
+++ pong.js
@@ -7,8 +7,8 @@ var p1V, p2V; //controlling the speed of the paddles
 // setup is just run once
 function setup() {
   createCanvas(600, 400);
-  p1 = height / 2 - 50; //This holds coordinates. Subtracting the height from half of the paddle so that the paddles start in the middle
-  p2 = height / 2 - 50;
+  p1 = p2 = height / 2 - 50; //This holds coordinates. Subtracting the height from half of the paddle so that the paddles start in the middle
+  p1V = p2V = 0;
 }
 
 // This is run 60 times per second, as fast as it can go
@@ -19,13 +19,8 @@ function draw() {
   rect(20, p1, 10, 100);
   rect(width - 20, p2, 10, 100);
 
-  //Need to constrain the paddles
-  p1 = constrain(p1, 0, height - 100);
-  p2 = constrain(p2, 0, height - 100);
-
-
-//handle inputs
-handleMovement();
+  //handle paddles
+  handleMovement();
 
 }
 
@@ -35,21 +30,37 @@ function handleMovement() {
 
   //player one controls
   if(keyIsDown(87)){
+    console.log('Why is this bat not moving');
+
     //move up
-    p1V =+ 5;
+    p1V -= 5;
   } else if(keyIsDown(83)) {
     //move down
-    p1V =- 5;
+    p1V += 5;
   }
 
   //player two controls
   if(keyIsDown(UP_ARROW)){
     //move up
-    p2V =+ 5;
+    p2V -= 5;
   } else if(keyIsDown(DOWN_ARROW)) {
     //move down
-    p2V =- 5;
+    p2V += 5;
   }
 
+
+  //Slows the paddles down and gives the illusion of friction
+
+  p1 += p1V;
+  p2 += p2V;
+
+  p1V *= 0.4;
+  p2V *= 0.4;
+
+
+
+  //Need to constrain the paddles
+  p1 = constrain(p1, 0, height - 100);
+  p2 = constrain(p2, 0, height - 100);
 
 }
