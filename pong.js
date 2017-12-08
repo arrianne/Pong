692c8a2ff9b123315877bc71b376fe42f9fbc1c2
arrianne <arrianneoshea@hotmail.co.uk>
2017-12-08 12:18:27 +0000

    Assigning keys to the up and down movement of the paddles and starting to add velocity to that movement

--- pong.js
+++ pong.js
@@ -1,5 +1,5 @@
-var paddle1, paddle2;
-
+var p1, p2;
+var p1V, p2V; //controlling the speed of the paddles
 
 
 // p5 will look for a function named setup
@@ -7,8 +7,8 @@ var paddle1, paddle2;
 // setup is just run once
 function setup() {
   createCanvas(600, 400);
-  paddle1 = height / 2 - 50; //This holds coordinates. Subtracting the height from half of the paddle so that the paddles start in the middle
-  paddle2 = height / 2 - 50;
+  p1 = height / 2 - 50; //This holds coordinates. Subtracting the height from half of the paddle so that the paddles start in the middle
+  p2 = height / 2 - 50;
 }
 
 // This is run 60 times per second, as fast as it can go
@@ -16,10 +16,40 @@ function draw() {
   background(51);
 
   //This creates the actual paddle shape, x axis not needed for up and down motion
-  rect(20, paddle1, 10, 100);
-  rect(width - 20, paddle2, 10, 100);
+  rect(20, p1, 10, 100);
+  rect(width - 20, p2, 10, 100);
 
   //Need to constrain the paddles
-  paddle1 = constrain(paddle1, 0, height - 100);
-  paddle2 = constrain(paddle2, 0, height - 100);
+  p1 = constrain(p1, 0, height - 100);
+  p2 = constrain(p2, 0, height - 100);
+
+
+//handle inputs
+handleMovement();
+
+}
+
+
+//p5 allows you to check if a key is down or not and controls the movement
+function handleMovement() {
+
+  //player one controls
+  if(keyIsDown(87)){
+    //move up
+    p1V =+ 5;
+  } else if(keyIsDown(83)) {
+    //move down
+    p1V =- 5;
+  }
+
+  //player two controls
+  if(keyIsDown(UP_ARROW)){
+    //move up
+    p2V =+ 5;
+  } else if(keyIsDown(DOWN_ARROW)) {
+    //move down
+    p2V =- 5;
+  }
+
+
 }
